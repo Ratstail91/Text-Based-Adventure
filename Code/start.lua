20bc099779b804fbe467df0ed7c60626c74a60f6
Chris Browne <yoda2031@gmail.com>
2012-11-09 01:41:06 +0100

    Now lists available games before starting one.

--- Code/start.lua
+++ Code/start.lua
@@ -6,7 +6,21 @@ local function file_exists(fname)
 	if f~=nil then io.close(f) return true else return false end
 end
 
+-- Lua implementation of PHP scandir function
+-- copied from http://stackoverflow.com/questions/5303174/get-list-of-directory-in-a-lua
+-- modified slightly (made it Windows-compatible)
+function scandir(directory)
+    local i, t, popen = 0, {}, io.popen
+    for filename in popen('dir "'..directory..'"'):lines() do
+        i = i + 1
+        t[i] = filename
+    end
+    return t
+end
 
+dir = {}
+dir.engine = "Engine/"
+dir.games = "Game/"
 
 --probably improve this later
 if not file_exists("start.lua") or not file_exists("Engine/main") then
@@ -15,14 +29,12 @@ if not file_exists("start.lua") or not file_exists("Engine/main") then
 end
 
 
+io.write("Available Games:\n")
+for g,k in scandir(dir.games) do io.write(k .. "\n") end
+
 
 io.write("Enter game name: ")
 local name = io.read()
-
-
-dir = {}
-dir.engine = "Engine/"
-dir.game = "Game/" .. name .. "/"
-
+dir.game = dir.games .. name .. "/"
 
 dofile(dir.engine .. "main")
