517bdc51ba2ae359dbde6c41a0c1619706c0168f
Kayne Ruse <ratstail91@yahoo.com.au>
2012-11-10 21:31:09 +1100

    Rolled back this file to a point where it was functional

--- Code/start.lua
+++ Code/start.lua
@@ -6,21 +6,7 @@ local function file_exists(fname)
 	if f~=nil then io.close(f) return true else return false end
 end
 
--- Lua implementation of PHP scandir function
--- copied from http://stackoverflow.com/questions/5303174/get-list-of-directory-in-a-lua
--- modified slightly (made it Windows-compatible)
-function scandir(directory)
-    local i, t, popen = 0, {}, io.popen
-    for filename in popen('dir "'..directory..'"'):lines() do
-        i = i + 1
-        t[i] = filename
-    end
-    return t
-end
 
-dir = {}
-dir.engine = "Engine/"
-dir.games = "Game/"
 
 --probably improve this later
 if not file_exists("start.lua") or not file_exists("Engine/main") then
@@ -29,12 +15,14 @@ if not file_exists("start.lua") or not file_exists("Engine/main") then
 end
 
 
-io.write("Available Games:\n")
-for g,k in scandir(dir.games) do io.write(k .. "\n") end
-
 
 io.write("Enter game name: ")
 local name = io.read()
-dir.game = dir.games .. name .. "/"
+
+
+dir = {}
+dir.engine = "Engine/"
+dir.game = "Game/" .. name .. "/"
+
 
 dofile(dir.engine .. "main")
