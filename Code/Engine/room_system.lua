994a7b205195484c3f978368418eac32ac4ea8bc
Kayne Ruse <ratstail91@yahoo.com.au>
2012-12-02 21:33:28 +1100

    Added table protection, added room deletion functions

--- Code/Engine/room_system.lua
+++ Code/Engine/room_system.lua
@@ -1,6 +1,9 @@
 --[[
 
-	This is the room system
+	This is the room system. It's designed so that no room can be lost,
+	or removed from the list without it being deleted. This is because
+	rooms should be created at the beginning of a scenario, and exist
+	until the end.
 
 	see Pseudocode/Room System.txt
 
@@ -8,26 +11,32 @@
 
 
 Room = {
-	prototype = {
+	prototype = tableprotect({
 		name = "No Name", --new rooms should *always* have unique names
 		desc = "No Description", --and descriptions
 		itemlist = {},
 		doorlist = {}
-	},
+	}),
 	list = {} --the global list of rooms
 }
 
 
 --Note: This might be a bit round-a-bout, but you can add an anonymous room without deleting previous ones.
-setmetatable(Room.list, {__index = function(t, key)
-	for k, v in pairs(Room.list) do
-		if key == v.name then return v end
+setmetatable(Room.list, {
+	__index = function(t, k)
+		for _, v in pairs(Room.list) do
+			if k == v.name then return v end
+		end
+	end,
+	__newindex = function(t, k, v)
+		error("Attempting to assign room " .. tostring(k),2)
 	end
-end})
+})
 
 
 function Room:new(arg)
-	local o = deepcopy(self.prototype)
+	--add a single room
+	local o = deepcopy(getmetatable(self.prototype).__index)
 	if type(arg) == "table" then --copy args into this room
 		for k, v in pairs(arg) do
 			o[deepcopy(k)] = deepcopy(v)
@@ -37,3 +46,38 @@ function Room:new(arg)
 	return o
 end
 
+
+function Room:delete(arg)
+	--delete a single room
+	if type(arg) == 'nil' then
+		table.remove(Room.list) --no args
+		return
+	end
+
+	if type(arg) == 'number' then
+		table.remove(Room.list, arg)
+		return
+	end
+
+	if type(arg) == 'string' then
+		Room:delete(Room.list[arg]) --reuse this function
+		return
+	end
+
+	if type(arg) == 'table' then
+		for k, v in pairs(Room.list) do
+			if arg == v then table.remove(Room.list, k) end
+		end
+		return
+	end
+
+	error("Unknown argument type", 2)
+end
+
+
+function Room:clear()
+	--delete all rooms
+	mt = getmetatable(Room.list)
+	Room.list = {}
+	setmetatable(Room.list, mt)
+end
